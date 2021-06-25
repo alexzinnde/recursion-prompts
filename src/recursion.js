3ab1ec3bfb52d5600d20068b55b269818a07fbdc
Alexander Zinn <alexzinnde@gmail.com>
2021-06-25 12:39:13 -0400

    10. palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -107,7 +107,13 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length <= 1) return true;
+  return string[0].toLowerCase() === string[string.length - 1].toLowerCase() ?
+          palindrome(string.substring(1, string.length - 1)) : false;
 };
+
+console.log(palindrome('o'));
+console.log(palindrome('racecar'));
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
