e245fec28d4d36ad137fa7c8cc2063bdbe827962
Alexander Zinn <alexzinnde@gmail.com>
2021-06-25 13:29:44 -0400

    15. compareStr

--- src/recursion.js
+++ src/recursion.js
@@ -161,6 +161,12 @@ var gcd = function (x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function (str1, str2) {
+  if (str1 === '' && str2 === '') return true;
+  if (str1.length === 1 && str2.length === 1) {
+    return str1 === str2;
+  }
+
+  return str1[0] === str2[0] ? compareStr(str1.substring(1), str2.substring(1)) : false;
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
